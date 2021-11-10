b941371d2a2c00683738adfc17b6a90d38070f20
epievalex <epievalex@gmail.com>
2021-11-10 20:09:11 +0300

    Update 1.24.rkt

--- chapter1/1.24.rkt
+++ chapter1/1.24.rkt
@@ -1,5 +1,5 @@
 #lang sicp
-(define times-to-test 1000000.0)
+(define times-to-test 1000)
 
 (define (square a)
   (* a a))
@@ -16,7 +16,7 @@
 (define (fermat-test n)
   (define (try-it a)
     (= (expmod a n n) a))
-  (try-it (+ 90 (random 10))))
+  (try-it (random 10000)))
 
 (define (fast-prime? n times)
   (cond ((= times 0) true)
@@ -30,8 +30,8 @@
   (newline)
   (display n)
   (display " *** ")
-  (display elapsed-time)
-  (display "ms "))
+  (display (/ elapsed-time 1000.0))
+  (display "ms"))
 
 (define (start-prime-test n start-time count)
   (cond
@@ -39,7 +39,7 @@
      (newline))
     
     ((prime? n)
-         (report-prime n (/ (- (runtime) start-time) times-to-test))
+         (report-prime n (- (runtime) start-time))
          (start-prime-test (+ n 1) (runtime) (+ 1 count)))
     (else (start-prime-test (+ n 1) (runtime) count))))
 
@@ -50,32 +50,29 @@
   (timed-primed-test start))
 
 (search-for-primes 1000)
-  
 (search-for-primes 1000000)
-
 (search-for-primes 1000000000000)
-
 (search-for-primes 1000000000000000000000000)
 
 ;According to growth order O(log n), we should to make 2 times more calculations for n^2 then for just n,
 ;but it doesn't mean that time for calculation itself is considered. In other words 0(log n) growth order
 ;shows only amount of steps, but not time needed for calculation, for this, most likely there is a definition,
 ;depending on the type of processor, performance, etc.
-
+;
 ;Below is a list of calculations, where each next call has a number that is the square of the previous one.
-
-;1009 *** 1.511189ms 
-;1013 *** 1.533599ms 
-;1019 *** 1.578705ms
-
-;1000003 *** 2.348156ms 
-;1000033 *** 2.394838ms 
-;1000037 *** 2.421136ms
-
-;1000000000039 *** 7.88598ms 
-;1000000000061 *** 8.272988ms 
-;1000000000063 *** 8.165992ms
-
-;1000000000000000000000007 *** 27.001549ms 
-;1000000000000000000000049 *** 26.540861ms 
-;1000000000000000000000121 *** 28.787124ms
+;
+;10007 *** 3.654ms
+;10009 *** 3.067ms
+;10037 *** 3.742ms
+;
+;1000003 *** 4.369ms
+;1000033 *** 4.844ms
+;1000037 *** 4.288ms
+;
+;1000000000039 *** 20.501ms
+;1000000000061 *** 17.14ms
+;1000000000063 *** 16.281ms
+;
+;1000000000000000000000007 *** 54.569ms
+;1000000000000000000000049 *** 48.737ms
+;1000000000000000000000121 *** 53.985ms
