0c3ddf1009282f2493b5e9fd5ca2e1c4e57de4bc
epievalex <epievalex@gmail.com>
2021-11-24 15:07:53 +0300

    1.29 solution refactoring

--- chapter1/1.29.rkt
+++ chapter1/1.29.rkt
@@ -1,13 +1,10 @@
-#lang racket
+#lang sicp
 
-(define (even? x)
-  (= (remainder x 2) 0))
+(define (inc x) (+ x 1))
 
-(define (inc x)
-  (+ x 1))
+(define (even? x) (= (remainder x 2) 0))
 
-(define (cube x)
-  (* x x x))
+(define (cube x) (* x x x))
 
 (define (sum term a next b)
   (if (> a b)
@@ -15,23 +12,26 @@
       (+ (term a)
          (sum term (next a) next b))))
 
-(define (integral f a b dx)
-  (define (add-dx x) (+ x dx))
-  (* (sum f (+ a (/ dx 2)) add-dx b)
-     dx))
-
-(define (simpson-integration f a b n)
+(define (simpsons-intergration f a b n)
   (define h (/ (- b a) n))
-  (define (term k)
-  (define y (+ a (* k h)))
-    (* (f y)(cond
-      ((or (= k 0) (= k n)) 1)
-      ((even? k) 2)
-      (else 4))))
-  (* (sum term 0 inc n) (/ h 3)))
-  
-(integral cube 0 1 0.01)
-(simpson-integration cube 0 1.0 100)
+
+  (define (yk idx)
+    (+ a (* idx h)))
+
+  (define (term idx)
+    (* (cond
+         ((= idx 0) 1)
+         ((= idx n) 1)
+         ((even? idx) 2)
+         (else 4))
+       (f (yk idx))))
+
+  (* (/ h 3)
+     (sum term 0 inc n)))
+
+(simpsons-intergration cube 0 1.0 100)
+(simpsons-intergration cube 0 1.0 1000)
+
 ;Result
 ;1st formula gives 0.24998750000000042
 ;Simpson's integration for n=100 gives 0.24999999999999992
