363e117fecf6e4e20e1d20b150f8a6787e00d5b5
epievalex <epievalex@gmail.com>
2021-11-26 18:16:32 +0300

    Update chapter1/1.28 solution
    Let the prime? procedure take values less
    than 2.
    
    As a result, prime? procedure call with argument
    that less than 2 doesn't cause a critical error.

--- chapter1/1.28.rkt
+++ chapter1/1.28.rkt
@@ -1,5 +1,4 @@
 #lang racket
-(require racket/trace)
 
 (define times-to-test 1000)
 
@@ -21,13 +20,16 @@
 (define (miller-rabin-test n)
   (define (try-it a)
     (= (expmod a (- n 1) n) 1))
-  (try-it (+ 1 (random (- n 1)))))
+  (if (< n 2)
+      false
+      (try-it (+ 1 (random (- n 1))))))
 
 (define (fast-prime? n times)
   (cond ((= times 0) true)
         ((miller-rabin-test n) (fast-prime? n (- times 1)))
         (else false)))
 
+(provide prime?)
 (define (prime? n)
   (fast-prime? n times-to-test))
 
